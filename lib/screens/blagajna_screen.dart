4088395260def0c132678c499aba096d34bc7e46
MajaR46 <maja.rotovnik002@gmail.com>
2024-09-15 20:37:33 +0200

    added categories to blagajna screen

--- lib/screens/blagajna_screen.dart
+++ lib/screens/blagajna_screen.dart
@@ -19,6 +19,7 @@ class _BlagajnaScreenState extends State<BlagajnaScreen> {
   bool isLoading = true;
   bool hasError = false;
   Map<String, List<dynamic>> categorizedItems = {};
+  String selectedCategory = "Vse";
 
   @override
   void initState() {
@@ -73,83 +74,199 @@ class _BlagajnaScreenState extends State<BlagajnaScreen> {
     return categories;
   }
 
+//filtriraj izdelke glede na kategorijo
+  List<dynamic> _getFilteredItems() {
+    if (selectedCategory == "Vse") {
+      return cafeItems;
+    } else {
+      return categorizedItems[selectedCategory] ?? [];
+    }
+  }
+
+//category list na vrhu zaslona
+  Widget _categoryList() {
+    List<String> categories = ["Vse"] + categorizedItems.keys.toList();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SizedBox(
+        height: 50,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: ((context, index) {
+            String category = categories[index];
+
+            int categoryIndex =
+                categorizedItems.keys.toList().indexOf(category);
+
+            Color assignedBackgroundColor =
+                backgroundColors[categoryIndex % backgroundColors.length];
+            Color assignedTextColor =
+                textColors[categoryIndex % textColors.length];
+
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedCategory = category;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: assignedBackgroundColor,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        category,
+                        style: AppStyles.button2
+                            .copyWith(color: assignedTextColor),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+//glavni seznam izdelkov
+  Widget _buildItemList() {
+    List<dynamic> filteredItems = _getFilteredItems();
+
+    if (filteredItems.isEmpty) {
+      return const Center(child: Text("No items available"));
+    }
+
+    if (selectedCategory == "Vse") {
+      Map<String, List<dynamic>> itemsByCategory =
+          _categorizeItems(filteredItems);
+
+      List<String> categories = categorizedItems.keys.toList();
+
+      return SizedBox(
+        height: 400,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: ((context, index) {
+              String category = categories[index];
+              List<dynamic> items = categorizedItems[category] ?? [];
+
+              return SizedBox(
+                width: 200,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        category,
+                        style: AppStyles.heading4,
+                      ),
+                    ),
+                    Expanded(
+                        child: ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (context, itemIndex) {
+                        final item = items[itemIndex];
+
+                        String category = item['category'];
+
+                        int categoryIndex =
+                            categorizedItems.keys.toList().indexOf(category);
+
+                        Color assignedBackgroundColor = backgroundColors[
+                            categoryIndex % backgroundColors.length];
+                        Color assignedTextColor =
+                            textColors[categoryIndex % textColors.length];
+
+                        return ItemCard(
+                            itemName: item['name'],
+                            itemPrice: item['price'],
+                            itemCategory: item['category'],
+                            backgroundColor: assignedBackgroundColor,
+                            textColor: assignedTextColor);
+                      },
+                    ))
+                  ],
+                ),
+              );
+            })),
+      );
+    } else {
+      //layout za posamezno kategorijo
+      return ListView.builder(
+        itemCount: filteredItems.length,
+        itemBuilder: (context, index) {
+          final item = filteredItems[index];
+          String category = item['category'];
+
+          int categoryIndex = categorizedItems.keys.toList().indexOf(category);
+
+          Color assignedBackgroundColor =
+              backgroundColors[categoryIndex % backgroundColors.length];
+          Color assignedTextColor =
+              textColors[categoryIndex % textColors.length];
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ItemCard(
+              itemName: item['name'],
+              itemPrice: item['price'],
+              itemCategory: item['category'],
+              backgroundColor: assignedBackgroundColor,
+              textColor: assignedTextColor,
+            ),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     String formattedDate = DateFormat("EEE, dd. MMM yyyy").format(currentDate);
     String formattedTime = DateFormat("HH:mm").format(currentDate);
 
     return Scaffold(
-      backgroundColor: AppStyles.grey,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppStyles.white,
-        iconTheme: const IconThemeData(color: AppStyles.blue),
-        title: Text(
-          formattedDate,
-          style: AppStyles.boldanparagraph1.copyWith(color: AppStyles.blue),
+        backgroundColor: AppStyles.grey,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: AppStyles.white,
+          iconTheme: const IconThemeData(color: AppStyles.blue),
+          title: Text(
+            formattedDate,
+            style: AppStyles.boldanparagraph1.copyWith(color: AppStyles.blue),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Text(
+                formattedTime,
+                style:
+                    AppStyles.boldanparagraph1.copyWith(color: AppStyles.blue),
+              ),
+            )
+          ],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Text(
-              formattedTime,
-              style: AppStyles.boldanparagraph1.copyWith(color: AppStyles.blue),
-            ),
-          )
-        ],
-      ),
-      drawer: const CustomDrawer(),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : hasError
-              ? const Center(child: Text('Failed to load data'))
-              : ListView(
-                  children: categorizedItems.entries.map((entry) {
-                    String category = entry.key;
-                    List<dynamic> items = entry.value;
-
-                    int categoryIndex =
-                        categorizedItems.keys.toList().indexOf(category);
-                    Color assignedBackgroundColor = backgroundColors[
-                        categoryIndex %
-                            backgroundColors
-                                .length]; // & omogoča da se loop ponovi
-
-                    Color assignedTextColor =
-                        textColors[categoryIndex % textColors.length];
-
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 110,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: items.length,
-                            itemBuilder: (context, index) {
-                              final sortedItems = List.from(items)
-                                ..sort((item1, item2) =>
-                                    (item1['name'] as String)
-                                        .compareTo(item2['name'] as String));
-
-                              final item = sortedItems[index];
-                              return Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 8.0),
-                                  child: ItemCard(
-                                    itemName: item['name'],
-                                    itemCategory: item['category'],
-                                    itemPrice: item['price'],
-                                    backgroundColor: assignedBackgroundColor,
-                                    textColor: assignedTextColor,
-                                  ));
-                            },
-                          ),
-                        ),
-                      ],
-                    );
-                  }).toList(),
-                ),
-    );
+        drawer: const CustomDrawer(),
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : hasError
+                ? const Center(child: Text('Failed to load data'))
+                : Column(
+                    children: [
+                      _categoryList(),
+                      Expanded(child: _buildItemList())
+                    ],
+                  ));
   }
 }
