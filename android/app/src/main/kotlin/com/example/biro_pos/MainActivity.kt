1051193160b636d044ef27de5c668793c043e123
MajaR46 <maja.rotovnik002@gmail.com>
2026-01-13 17:58:05 +0100

    popravljen bluetooth v5.28.3

--- android/app/src/main/kotlin/com/example/biro_pos/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/biro_pos/MainActivity.kt
@@ -22,6 +22,9 @@ import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import androidx.activity.enableEdgeToEdge
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "bluetooth_channel"
@@ -688,22 +691,29 @@ class MainActivity : FlutterActivity() {
             result.error("NO_DEVICE", "No device connected.", null)
         }
     }
+private var keepAliveExecutor: ScheduledExecutorService? = null
 
-    private fun startKeepAlive() {
-        Thread {
-                    while (bluetoothSocket?.isConnected == true) {
-                        try {
-                            outputStream?.write(byteArrayOf(0x00)) // Pošlje prazen bajt
-                            outputStream?.flush()
-                            Thread.sleep(60000) // Počaka 2 minuti
-                        } catch (e: IOException) {
-                            Log.e("Bluetooth", "KeepAlive error", e)
-                            break
-                        }
-                    }
-                }
-                .start()
-    }
+private fun startKeepAlive() {
+    stopKeepAlive()
+
+    keepAliveExecutor = Executors.newSingleThreadScheduledExecutor()
+    keepAliveExecutor?.scheduleAtFixedRate({
+        try {
+            if (bluetoothSocket?.isConnected == true) {
+                outputStream?.write(byteArrayOf(0x00))
+                outputStream?.flush()
+            }
+        } catch (e: IOException) {
+            Log.e("Bluetooth", "KeepAlive error", e)
+            stopKeepAlive()
+        }
+    }, 1, 60, TimeUnit.SECONDS)
+}
+
+private fun stopKeepAlive() {
+    keepAliveExecutor?.shutdownNow()
+    keepAliveExecutor = null
+}
 
     // ----------------------------------USB---------------------------------------------------
     private fun connectUsbPrinter(result: MethodChannel.Result) {
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -58,7 +58,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   List<Blagajna> blagajna = [];
   final BluetoothService _bluetoothService = BluetoothService();
   String? lastRefresh;
-  String verzijaPrograma = '5.28.2';
+  String verzijaPrograma = '5.28.3';
   String formattedDate = '';
   String formattedTime = '';
   late Timer _timer;
