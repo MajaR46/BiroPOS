16e9b7d1bf07cc3af9d3d843224e66ee0d2b928a
MajaR46 <maja.rotovnik002@gmail.com>
2026-05-10 09:50:30 +0200

    v 5.30.8

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -59,7 +59,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   List<Blagajna> blagajna = [];
   final BluetoothService _bluetoothService = BluetoothService();
   String? lastRefresh;
-  String verzijaPrograma = '5.30.7';
+  String verzijaPrograma = '5.30.8';
   String formattedDate = '';
   String formattedTime = '';
   late Timer _timer;
